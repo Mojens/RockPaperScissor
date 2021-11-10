85427462e283f18fc2e8a9bc662219fc964409ee
Mojens <moha135h@stud.kea.dk>
2021-11-10 14:43:31 +0100

    Fix rundt

--- src/Computer.java
+++ src/Computer.java
@@ -1,10 +1,7 @@
-import java.util.Random;
 public class Computer implements Player {
 
   public Enum takeTurn() {
     int rand = (int) (Math.random() * 3);
-    //Convert the random number to a string using conditionals and print the opponent's move
-    String opponentMove = "";
     if (rand == 0) {
       return Choice.STEN;
     } else if (rand == 1) {
--- src/Human.java
+++ src/Human.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Human implements Player{
+public class Human implements Player {
   @Override
   public Enum takeTurn() {
     Scanner input = new Scanner(System.in);
     String playerInput = input.nextLine();
 
-    if (playerInput.equalsIgnoreCase("Sten")){
+    if (playerInput.equalsIgnoreCase("Sten")) {
       return Choice.STEN;
-    }else if (playerInput.equalsIgnoreCase("Papir")){
+    } else if (playerInput.equalsIgnoreCase("Papir")) {
       return Choice.PAPIR;
-    }else
+    } else
       return Choice.SAKS;
   }
 }
